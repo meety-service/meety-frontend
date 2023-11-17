eb87e97c8c717fcccf2c56e180e143a08952e1d5
2019014320 <ckjsjs@hanyang.ac.kr>
2023-11-17 15:40:33 +0900

    Feat: Axios 패키지 설치

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Navbar, MainPage, LoginPage, MeetingCreatePage, MeetingFillPage, MeetingViewPage, VoteCreatePage, VoteFillPage, VoteViewPage, MeetingConfirmedPage, ErrorPage } from './component';
 
+function setScreenSize() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+useEffect(() => {
+  setScreenSize();
+});
+
 function App() {
   return (
     <div className="App">
