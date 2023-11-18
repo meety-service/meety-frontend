b5a7cdd6526c0a042fb0d1668758326da1dc9c68
2019014320 <ckjsjs@hanyang.ac.kr>
2023-11-18 18:16:39 +0900

    Feat: 로그인 페이지 초기 구현
    - 로그인 페이지 및 로그인 버튼 구현 (LoginPage.jsx,
      GoogleLoginButton.jsx)
    - 토큰 ID 요청 로직 구현 (react-oauth/google 사용)
    - env 파일에 ClientID 추가 (gitignore로 숨김 처리)

--- src/component/LoginPage.jsx
+++ src/component/LoginPage.jsx
@@ -1,20 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import GoogleLoginButton from './GoogleLoginButton';
 
 const LoginPage = () => {
-    const [fadeIn, setFadeIn] = useState(false);
-
-    useEffect(() => {
-      // 페이지가 로드될 때 페이드 인 효과 시작
-      setFadeIn(true);
-    }, []);
-
   return (
     <div
-      className={"nav_top_padding mobile_h_fit w-screen h-screen"}
+      className={"mobile_h_fit w-screen h-screen"}
     >
-      <div className="bg-[url('./assets/login_bg_image_sm.jpg')] md:bg-[url('./assets/login_bg_image_lg.jpg')] bg-cover h-full">
-        <div className="h-full w-full bg-black opacity-50">
-            
+      <div className="relative top-0 left-0 bg-[url('./assets/login_bg_image_sm.jpg')] md:bg-[url('./assets/login_bg_image_lg.jpg')] h-full bg-cover">
+        <div className="absolute h-full w-full bg-black opacity-50" />
+        <div className="absolute h-full w-full flex flex-col justify-center items-center text-2xl font-extrabold">
+            <p className="text-white">당신의 모든 약속</p>
+            <p className="text-white">미티(Meety)로 시작하세요</p>
+        </div>
+        <div className="absolute bottom-0 pb-4 w-full flex flex-row justify-center">
+          <GoogleLoginButton />
         </div>
       </div>
     </div>
