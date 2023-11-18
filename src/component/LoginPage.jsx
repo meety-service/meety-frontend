7e0a553e09e7e41b3b442763ad9878ff6cfc1c79
2019014320 <ckjsjs@hanyang.ac.kr>
2023-11-18 16:40:17 +0900

    Feat: 로그인 페이지 구현

--- src/component/LoginPage.jsx
+++ src/component/LoginPage.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
 const LoginPage = () => {
-    return (
-        <div>Login</div>
-    )
-}
+    const [fadeIn, setFadeIn] = useState(false);
 
-export default LoginPage
+    useEffect(() => {
+      // 페이지가 로드될 때 페이드 인 효과 시작
+      setFadeIn(true);
+    }, []);
+
+  return (
+    <div
+      className={"nav_top_padding mobile_h_fit w-screen h-screen"}
+    >
+      <div className="bg-[url('./assets/login_bg_image_sm.jpg')] md:bg-[url('./assets/login_bg_image_lg.jpg')] bg-cover h-full">
+        <div className="h-full w-full bg-black opacity-50">
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
