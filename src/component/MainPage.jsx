bf60658237fbc2cab1485174198752d5b8784ee8
2019014320 <ckjsjs@hanyang.ac.kr>
2023-11-19 16:45:46 +0900

    Feat: 메인 페이지 초기 구현
    - 메인 페이지 내 스크롤 바 구현
    - 미팅 폼 생성 페이지로 이동 버튼 구현

--- src/component/MainPage.jsx
+++ src/component/MainPage.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import PageTitle from "./PageTitle";
+import { useNavigate } from "react-router-dom";
+import GradationButton from "./GradationButton";
+
+const parsedMeetingInfo = {
+  meetings: [
+    { id: 3, name: "소공 1차 회의", isMaster: 0 },
+    { id: 7, name: "프로젝트 2차 설계회의", isMaster: 1 },
+    { id: 7, name: "프로젝트 2차 설계회의", isMaster: 1 },
+    { id: 7, name: "프로젝트 2차 설계회의", isMaster: 1 },
+    { id: 7, name: "프로젝트 2차 설계회의", isMaster: 1 },
+    { id: 7, name: "프로젝트 2차 설계회의", isMaster: 1 },
+    { id: 7, name: "프로젝트 2차 설계회의", isMaster: 1 },
+    { id: 7, name: "프로젝트 2차 설계회의", isMaster: 1 },
+  ],
+};
 
 const MainPage = () => {
-  return (
-    <div className="nav_top_padding">MainPage</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default MainPage
+  return (
+    <div className="nav_top_padding mobile_h_fit bg-white w-screen h-screen">
+      <div className="relative flex flex-col justify-center items-center w-full h-full">
+        <div className="absolute top-8 left-4">
+          <PageTitle title="내 미팅" />
+        </div>
+        <div className="w-full h-full flex flex-col justify-center items-center px-5 pt-20 pb-28">
+          {/*옵션 스크롤 바*/}
+          <div className="w-full md:w-2/5 h-full py-3 px-3 bg-stone-400 shadow-md shadow-stone-400 rounded-lg">
+            <div className="w-full h-full space-y-4 overflow-y-scroll rounded-lg">
+              {parsedMeetingInfo.meetings.map((meetingInfo) => (
+                <div
+                  key={meetingInfo.id}
+                  className="h-16 w-full flex flex-row items-center justify-center bg-gradient-to-r rounded-2xl shadow-md border-2 border-solid border-meety-component_outline_gray mx-auto bg-white"
+                >
+                  <p className="text-lg font-extrabold">{meetingInfo.name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="absolute h-[60px] w-full bottom-0 left-0">
+          <div className="absolute bottom-0 px-5 mb-7 w-full h-full ">
+            <GradationButton
+              text={"새로운 미팅 생성하기"}
+              onButtonClick={() => navigate("/meeting/create")}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
