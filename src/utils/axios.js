868984a6357c7968e9afd93a19998b9db3425f39
2019014320 <ckjsjs@hanyang.ac.kr>
2023-11-23 16:51:58 +0900

    Feat: Header에 쿠키를 디폴트로 담은 axios 추가

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,4 +1,17 @@
 import axios from "axios";
+import { getCookie } from "./cookie";
+
+export const axiosWH = axios.create({
+  baseURL: "http://localhost:8080",
+  timeout: 5000,
+  headers: {
+   "Content-Type": "application/json",
+   "Access-Control-Allow-Origin": "*",
+   Cookie: `X-Gapi-Refresh-Token=${getCookie('X-Gapi-Refresh-Token')};`
+ },
+ withCredentials: true,
+ responseType: "json",
+});
 
 export const getMeetingForm = async (id) => {
   try {
