43636e502697eb02c77d10ec59d9520047ce5869
2019014320 <ckjsjs@hanyang.ac.kr>
2023-11-25 13:52:02 +0900

    Feat: 요일별 미팅 가능 시간대 정렬 기능 구현
    - 모든 미팅 셀 데이터를 입력 받아, 최소 시간을 기준으로 요일별 미팅 가능
      시간대를 계산(정렬)하는 함수 구현 (meetingSort.js)
    - dateParser에 startTime, endTime를 인자로 입력받아 "hh:mm ~ hh:mm" 형식의 문자열을
      반환하는 기능 추가 (dateParser.js)

--- src/utils/dateParser.js
+++ src/utils/dateParser.js
@@ -1,22 +1,48 @@
 /**
- * 주어진 날짜 및 시간 정보를 파싱하여 다음과 같은 문자열을 반환한다.
- *   (1) date, startTime, endTime 모두 입력된 경우 -> "YYYY년 M월 D일 (요일)  hh:mm ~ hh:mm"  
- *   (2) date 만 입력된 경우 -> "YYYY년 M월 D일 (요일)"  
- * @param {string} date - 날짜 문자열 (YYYY-MM-DD 형식)
- * @param {string} startTime - 시작 시간 문자열 (HH:mm:ss 형식, 옵션)
- * @param {string} endTime - 종료 시간 문자열 (HH:mm:ss 형식, 옵션)
+ * 주어진 날짜 및 시간 정보를 파싱하여 다음과 같은 문자열을 반환한다.  
+ *   (1) date만 입력된 경우 -> "YYYY년 M월 D일 (요일)"  
+ *   (2) startTime, endTime만 입력된 경우 -> "hh:mm ~ hh:mm"  
+ *   (3) date, startTime, endTime 모두 입력된 경우 -> "YYYY년 M월 D일 (요일)  hh:mm ~ hh:mm"  
+ * @param {...string} dateInfo - 날짜 관련 문자열들
  * @returns {string|undefined} - 파싱된 문자열 또는 오류 시 undefined 반환
  */
-export const dateParser = (date, startTime = undefined, endTime = undefined) => {
+export const dateParser = (...dateInfo) => {
+    const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
+    let dateString;
     try {
-        // date, startTime, endTime 입력 -> "YYYY년 M월 D일 hh:mm ~ hh:mm"
-        const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
-        let dateString;
-        if (startTime && endTime) {
+        // (1) date만 입력 -> "YYYY년 M월 D일"
+        if (dateInfo.length == 1) {
+            const date = dateInfo[0];
+            const onlyDate = new Date(Date.parse(date));
+            dateString = `${onlyDate.getFullYear()}년 ${onlyDate.getMonth() + 1
+                }월 ${onlyDate.getDate()}일 (${daysOfWeek[onlyDate.getDay()]})`
+        }
+        // (2) startTime, endTime 입력 -> "hh:mm ~ hh:mm"
+        else if (dateInfo.length == 2) {
+            const [hour1, minute1, second1] = dateInfo[0].split(":").map(Number);
+            const [hour2, minute2, second2] = dateInfo[1].split(":").map(Number);
+
+            const startTime = new Date(0, 0, 0, hour1, minute1, second1);
+            const endTime = new Date(0, 0, 0, hour2, minute2, second2);
+
+            dateString = `${String(
+                startTime.getHours()
+            ).padStart(2, "0")}:${String(startTime.getMinutes()).padStart(
+                2,
+                "0"
+            )} ~ ${String(endTime.getHours()).padStart(2, "0")}:${String(
+                endTime.getMinutes()
+            ).padStart(2, "0")}`;
+        }
+        // (3) date, startTime, endTime 입력 -> "YYYY년 M월 D일 hh:mm ~ hh:mm"
+        else if (dateInfo.length == 3) {
+            const date = dateInfo[0];
+            const startTime = dateInfo[1];
+            const endTime = dateInfo[2];
             const startDate = new Date(Date.parse(date + " " + startTime));
             const endDate = new Date(Date.parse(date + " " + endTime));
             dateString = `${startDate.getFullYear()}년 ${startDate.getMonth() + 1
-                }월 ${startDate.getDate()}일 (${daysOfWeek[startDate.getDay()]}) ${String(
+                }월 ${startDate.getDate()}일 (${daysOfWeek[startDate.getDay()]})  ${String(
                     startDate.getHours()
                 ).padStart(2, "0")}:${String(startDate.getMinutes()).padStart(
                     2,
@@ -25,14 +51,12 @@ export const dateParser = (date, startTime = undefined, endTime = undefined) => 
                     endDate.getMinutes()
                 ).padStart(2, "0")}`;
         } else {
-            // date만 입력 -> "YYYY년 M월 D일"
-            const onlyDate = new Date(Date.parse(date));
-            dateString = `${onlyDate.getFullYear()}년 ${onlyDate.getMonth() + 1
-                }월 ${onlyDate.getDate()}일 (${daysOfWeek[onlyDate.getDay()]})`
+            console.log(`[Parsing Error] ${dateInfo}`);
         }
+
         return dateString;
     } catch (e) {
-        console.log("Parsing Error!!!")
+        console.log(`[Parsing Error] ${e}`);
         return undefined;
     }
 };
---
+++ src/utils/meetingSort.js
@@ -0,0 +1,123 @@
+import { dateParser } from "./dateParser";
+
+/**
+ * "HH:mm:ss" 형식의 두 문자열을 입력받아 두 시간 사이의 밀리 초를 리턴하는 함수
+ * @param {string} startTime - 시작 시간 문자열
+ * @param {string} endTime - 종료 시간 문자열
+ * @returns {number} - 두 시간 사이의 밀리 초
+ */
+export const getMillisecondsBetweenTimes = (startTime, endTime) => {
+    // 시간 문자열을 "HH:mm:ss" 형식에서 추출
+    const [hour1, minute1, second1] = startTime.split(":").map(Number);
+    const [hour2, minute2, second2] = endTime.split(":").map(Number);
+
+    // Date 객체를 사용하여 두 시간을 밀리초로 변환
+    const date1 = new Date(0, 0, 0, hour1, minute1, second1);
+    const date2 = new Date(0, 0, 0, hour2, minute2, second2);
+
+    // 두 날짜 사이의 밀리초 차이 계산
+    const timeDifference = date2 - date1;
+
+    return timeDifference;
+};
+
+
+/**
+ * 밀리초를 입력받아 해당 시간 안에 총 몇 개의 셀이 존재하는지를 리턴하는 함수
+ * @param {number} millisec - 밀리 초
+ * @returns {number} - 셀 개수
+ */
+export const getCellCount = (millisec) => {
+    return millisec / (60000 * 15);  // x분 / 15 -> 15분 단위 셀의 개수를 리턴
+};
+
+
+/**
+ * 요일별 전체 미팅 셀 데이터, 미팅 시작 시간, 미팅 끝 시간, 최소 시간을 입력받아,
+ * 최소 시간을 기준으로 요일별 가장 많이 겹치는 시간대에 대한 데이터를 리턴하는 함수
+ * @param {object} input - 요일별 전체 미팅 셀 데이터 (Object)
+ * @param {string} startTime - 미팅 시작 시간 문자열 ("hh:mm:ss")
+ * @param {string} endTime - 미팅 끝 시간 문자열 ("hh:mm:ss")
+ * @param {number} minHour - 최소 시간 (Int)
+ * @returns {object} - 요일별 가장 많이 겹치는 시간대 데이터 (Object)
+ */
+export const getSortedMeetingInfo = (input, startTime, endTime, minHour) => {
+
+    const cellCount = getCellCount(getMillisecondsBetweenTimes(startTime, endTime));  // 총 셀 개수
+    const dayCount = input.schedules.length;  // 총 요일 수
+    const memberCount = input.members;        // 참여한 총 멤버 수
+    const minCellCount = minHour * 4;         // ex) 최소 1시간 -> 최소 셀 개수 : 4개 (15 * 4)
+
+    let intervalStart = "00:00:00";    // Interval 시작 시간
+    let intervalEnd = "00:00:00";      // Interval 종료 시간
+    let intervalExist = false;         // Interval 진행 중인지 여부
+    let continuousCellCount = 0;       // 연속된 셀 개수
+    let timeString = "";               // "hh:mm ~ hh:mm" 형식 문자열
+    let memRatioString = "";           // "m/n 명" 형식 문자열
+    let minMemberCount = memberCount;  // 최소 인원 수
+    let times = {};                    // schedule 내 모든 셀 객체 리스트
+
+    /* Iteration에서 사용된 변수 초기화 */
+    const initVars = () => {
+        intervalStart = "00:00:00";
+        intervalEnd = "00:00:00";
+        intervalExist = false;
+        continuousCellCount = 0;
+        timeString = "";
+        memRatioString = "";
+    }
+
+    // 리턴값
+    const result = {
+        members: memberCount,
+        schedules: []
+    };
+
+    // 초기 구조 설정
+    input.schedules.forEach((schedule) => {
+        result.schedules.push({ date: dateParser(schedule.date), cases: [] });
+    });
+
+    for (let dayIdx = 0; dayIdx < dayCount; dayIdx++) {
+        times = input.schedules[dayIdx].times;
+        minMemberCount = memberCount;
+        for (; minMemberCount > 0; minMemberCount--) {
+            for (let cellIdx = 0; cellIdx < cellCount; cellIdx++) {
+                // 아직 Interval이 시작하지 않았고, minMemberCount보다 많거나 같은 인원이 존재하는 경우 -> Interval 시작
+                if (!intervalExist && times[cellIdx].available.length >= minMemberCount) {
+                    intervalExist = true;
+                    intervalStart = times[cellIdx].time;
+                    continuousCellCount++;
+                }
+                // Interval이 시작되었고, minMemberCount보다 적은 인원이 존재하는 경우 -> Interval 종료
+                else if (intervalExist && times[cellIdx].available.length < minMemberCount) {
+
+                    // 연속된 셀 개수가 최소 셀 개수보다 크거나 같으면 저장
+                    if (continuousCellCount >= minCellCount) {
+                        intervalEnd = times[cellIdx].time;
+                        timeString = dateParser(intervalStart, intervalEnd);
+                        memRatioString = `${minMemberCount}/${memberCount} (명)`
+                        result.schedules[dayIdx].cases.push({ time: timeString, availMemRatio: memRatioString });
+                    }
+
+                    initVars();
+                }
+                // Interval이 시작되었고, memberCount보다 많거나 같은 인원이 존재하는 경우 -> Interval 계속
+                else if (intervalExist) {
+                    continuousCellCount++;
+                }
+            }
+
+            // 마지막 셀까지 Interval이 유지되는 경우 -> 조건(Interval 내 셀 개수가 최소 셀 개수 이상) 충족 시 저장
+            if (intervalExist && continuousCellCount >= minCellCount) {
+                intervalEnd = endTime;
+                timeString = dateParser(intervalStart, intervalEnd);
+                memRatioString = `${minMemberCount}/${memberCount} (명)`
+                result.schedules[dayIdx].cases.push({ time: timeString, availMemRatio: memRatioString });
+            }
+
+            initVars();
+        }
+    }
+    return result;
+};
